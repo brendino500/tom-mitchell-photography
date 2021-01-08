337a8bf560856a7be36107ea34c898a13f3049f8
brendino500 <brenda.ty@live.com>
2021-01-08 16:19:49 +0000

    bug fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,66 +56,6 @@ export default function Main() {
           😬
         </p>
       </div>
-      <div data-scroll-section>
-        <h1 data-scroll>Hey</h1>
-        <p data-scroll>👋</p>
-      </div>
-      <div data-scroll-section>
-        <h2 data-scroll data-scroll-speed="1">
-          What's up?
-        </h2>
-        <p data-scroll data-scroll-speed="2">
-          😬
-        </p>
-      </div>
-      <div data-scroll-section>
-        <h1 data-scroll>Hey</h1>
-        <p data-scroll>👋</p>
-      </div>
-      <div data-scroll-section>
-        <h2 data-scroll data-scroll-speed="1">
-          What's up?
-        </h2>
-        <p data-scroll data-scroll-speed="2">
-          😬
-        </p>
-      </div>
-      <div data-scroll-section>
-        <h1 data-scroll>Hey</h1>
-        <p data-scroll>👋</p>
-      </div>
-      <div data-scroll-section>
-        <h2 data-scroll data-scroll-speed="1">
-          What's up?
-        </h2>
-        <p data-scroll data-scroll-speed="2">
-          😬
-        </p>
-      </div>
-      <div data-scroll-section>
-        <h1 data-scroll>Hey</h1>
-        <p data-scroll>👋</p>
-      </div>
-      <div data-scroll-section>
-        <h2 data-scroll data-scroll-speed="1">
-          What's up?
-        </h2>
-        <p data-scroll data-scroll-speed="2">
-          😬
-        </p>
-      </div>
-      <div data-scroll-section>
-        <h1 data-scroll>Hey</h1>
-        <p data-scroll>👋</p>
-      </div>
-      <div data-scroll-section>
-        <h2 data-scroll data-scroll-speed="1">
-          What's up?
-        </h2>
-        <p data-scroll data-scroll-speed="2">
-          😬
-        </p>
-      </div>
     </div>
   );
 }
