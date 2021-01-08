efe795c8d624f6bb1333322892dd2cb101ff1c09
brendino500 <brenda.ty@live.com>
2021-01-08 16:07:00 +0000

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,121 @@
+import React from "react";
+import LocomotiveScroll from "locomotive-scroll";
+
+export default function Main() {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+  });
+  return (
+    <div data-scroll-container>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+      <div data-scroll-section>
+        <h1 data-scroll>Hey</h1>
+        <p data-scroll>👋</p>
+      </div>
+      <div data-scroll-section>
+        <h2 data-scroll data-scroll-speed="1">
+          What's up?
+        </h2>
+        <p data-scroll data-scroll-speed="2">
+          😬
+        </p>
+      </div>
+    </div>
+  );
+}
